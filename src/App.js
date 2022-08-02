f857d4233282e702da532820ee09bda3b66ec44d
sookyeongyeom <sookyeong.dev@gmail.com>
2022-08-03 07:05:56 +0900

    :books: p188

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ import EventPracticeFunc from './EventPracticeFunc';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
 // import { Fragment } from 'react';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
   render() {
     return (
       <>
@@ -31,6 +45,10 @@ class App extends Component {
         <br />
         <br />
         <IterationSample />
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+          <button onClick={this.handleClick}>랜덤 색상</button>
+        </ErrorBoundary>
       </>
     );
   }
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+// 자신의 children component에서 발생한 에러만 catch 할 수 있음
+class ErrorBoundary extends Component {
+  state = {
+    error: false,
+  };
+  componentDidCatch(error, info) {
+    this.setState({
+      error: true,
+    });
+    console.log({ error, info });
+  }
+  render() {
+    if (this.state.error) return <div>에러가 발생했습니다!</div>;
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
